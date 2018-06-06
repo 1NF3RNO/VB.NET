2ab4961317c1fdd01e688edb2070a11e5b20f98f
1NF3RNO <1NF3RNO@users.noreply.github.com>
2018-06-07 00:12:38 +1000

    some code

---
+++ Solution1/Class1.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Class1
+
+    'A record for holding Employee Data
+    Private Structure Employee
+        Dim strFirstName As String
+        Dim strSurname As String
+    End Structure
+
+    'Array to hold all of the Employees
+    Dim Employees(100) As Employee
+    Dim intEmployeeCount As Integer
+
+End Class
